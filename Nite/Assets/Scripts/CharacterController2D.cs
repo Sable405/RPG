d129b030510f300d5fd1646f17d9c5d81ccabb84
Sable405 <111007485+Sable405@users.noreply.github.com>
2024-05-16 12:55:05 -0700

    Yes

--- Nite/Assets/Scripts/CharacterController2D.cs
+++ Nite/Assets/Scripts/CharacterController2D.cs
@@ -2,16 +2,13 @@ using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
 {
-    //WalkR and WalkL are reversd :(
     private Animator animator;
     public bool WalkL = false;
     public bool WalkR = false;
     public bool WalkS = false;
     public bool WalkW = false;
-
-    // Multiple Directons
-    public bool WalkWD = false;
     public float speed;
+    public float baseSpeed = 5f;
     public float groundDist;
 
     public LayerMask terrainLayer;
@@ -22,6 +19,7 @@ public class CharacterController2D : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        speed = baseSpeed;
     }
 
     void Update()
@@ -44,8 +42,7 @@ public class CharacterController2D : MonoBehaviour
         Vector3 moveDir = new Vector3(x, 0, y);
         rb.velocity = moveDir * speed;
 
-        
-    //Walk Animations
+        // Walk Animations
         if (Input.GetKeyDown(KeyCode.A))
         {
             WalkR = true;
@@ -54,7 +51,6 @@ public class CharacterController2D : MonoBehaviour
         {
             WalkR = false;
         }
-
         if (animator != null) // Check if animator is assigned
         {
             animator.SetBool("WalkR", WalkR);
@@ -63,30 +59,24 @@ public class CharacterController2D : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             WalkL = true;
-            
         }
         else if (Input.GetKeyUp(KeyCode.D))
         {
             WalkL = false;
-            
         }
-
         if (animator != null) // Check if animator is assigned
         {
             animator.SetBool("WalkL", WalkL);
         }
 
-         if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             WalkS = true;
-            
         }
         else if (Input.GetKeyUp(KeyCode.S))
         {
             WalkS = false;
-            
         }
-
         if (animator != null) // Check if animator is assigned
         {
             animator.SetBool("WalkS", WalkS);
@@ -95,33 +85,26 @@ public class CharacterController2D : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
         {
             WalkW = true;
-            
         }
         else if (Input.GetKeyUp(KeyCode.W))
         {
             WalkW = false;
-            
         }
-
         if (animator != null) // Check if animator is assigned
         {
             animator.SetBool("WalkW", WalkW);
         }
 
         // Run Control
-          if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-           speed = speed * 1.5f;
-            animator.speed = speed; 
-
+            speed = baseSpeed * 1.5f;
+            animator.speed = 1.5f; // Set animator speed to 1.5 times the original
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-           speed = speed / 1.5f;
-           animator.speed = speed / 1.5f;
-            
+            speed = baseSpeed; 
+            animator.speed = 1f; 
         }
-    
     }
-
 }
